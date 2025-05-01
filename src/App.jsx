1677b145994ade93c8e90102ff5e9077cb43dbbb
SoloDesktop <solo213974@yahoo.com>
2025-05-01 17:44:34 -0400

    Saving to begin working on character page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 // /src/App.jsx
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Characters from './pages/Characters';
+import Explore from './pages/Explore';
 import Home from './pages/Home';
+import News from './pages/News';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/explore" element={<Explore />} />
         <Route path="/" element={<Home />} />
+        <Route path="/news" element={<News />} />
       </Routes>
     </BrowserRouter>
   );
